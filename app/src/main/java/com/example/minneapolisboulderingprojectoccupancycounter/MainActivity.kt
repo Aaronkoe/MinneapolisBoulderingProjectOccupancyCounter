1a1b78c50297e6f8a5c13a7cfb85386d75252d1b
Aaron Koenigsberg <koeni296@umn.edu>
2021-07-22 16:47:54 -0500

    Removing unnecessary code

--- app/src/main/java/com/example/minneapolisboulderingprojectoccupancycounter/MainActivity.kt
+++ app/src/main/java/com/example/minneapolisboulderingprojectoccupancycounter/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
@@ -42,10 +41,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        return true;
     }
 
     override fun onSupportNavigateUp(): Boolean {

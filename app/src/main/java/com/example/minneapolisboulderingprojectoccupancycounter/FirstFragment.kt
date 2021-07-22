9615603a553f16c7add4c5c22114a3f2e9bd2c7e
Aaron Koenigsberg <koeni296@umn.edu>
2021-07-22 16:15:09 -0500

    Adding refresh button, removing excess stuff, changing colors

--- app/src/main/java/com/example/minneapolisboulderingprojectoccupancycounter/FirstFragment.kt
+++ app/src/main/java/com/example/minneapolisboulderingprojectoccupancycounter/FirstFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import androidx.navigation.fragment.findNavController
 import com.example.minneapolisboulderingprojectoccupancycounter.databinding.FragmentFirstBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -35,6 +36,15 @@ class FirstFragment : Fragment() {
         if (occupancyWebView != null) {
             occupancyWebView.settings.javaScriptEnabled = true
             occupancyWebView.loadUrl("https://portal.rockgympro.com/portal/public/18a5ea0176c6494befd44f163f15750c/occupancy")
+        }
+
+        val fab = view?.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val occupancyWebView = view?.findViewById<WebView>(R.id.occupancyWebView)
+            if (occupancyWebView != null) {
+                occupancyWebView.settings.javaScriptEnabled = true
+                occupancyWebView.loadUrl("https://portal.rockgympro.com/portal/public/18a5ea0176c6494befd44f163f15750c/occupancy")
+            }
         }
     }
 
